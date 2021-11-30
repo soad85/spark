40ee4c7780dc5a5abd8fe42078f21a20dc785d21
soad85 <ariboya85@gmail.com>
2021-11-30 15:21:30 -0300

    heroku

--- src/main/java/pokemon/Server.java
+++ src/main/java/pokemon/Server.java
@@ -5,7 +5,15 @@ import spark.Spark;
 public class Server {
   public static void main(String[] args) {
     Bootstrap.init();
-    Spark.port(9000);
+    Spark.port(getHerokuAssignedPort());
     Router.configure();
+  }
+
+  static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 9000; // puerto por defecto
   }
 }
